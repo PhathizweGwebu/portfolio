21437f6491c5ebc28996990a35da14e78bd1636c
PhathizweGwebu <phathizwe808@gmail.com>
2023-10-05 16:19:07 +0200

    mobile friendly version of the website

--- index.js
+++
@@ -1,9 +0,0 @@
-const burgerMenu = document.querySelector(".fa-bars");
-const navItems = document.querySelector(".nav-list");
-
-
-
-burgerMenu.addEventListener("click", ()=>{
-    alert("clicked");
-    navItems.classList.toggle('active');
-})
